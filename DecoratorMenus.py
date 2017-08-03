ee286672bbc13bf0bca22d60e21879a848615b3f
PurposeCyberspace-Foundry <mdlombardo@liberty.edu>
2017-08-03 17:22:47 -0500

    Initial Commit

--- DecoratorMenus.py
+++ DecoratorMenus.py
@@ -1,17 +1,5 @@
-#DecoratorMenus
 """
-The MIT License
-
-Copyright 2017 Matthew Lombardo
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-"""
-
-"""
+DecoratorMenus
 This class allows the use of @decorators to create submenu-driven programs easily and quickly in python
 the menu_item options are all passed directly to the "add_submenu" argparse function
 the argument options are passed to the "add_argument" argparse function
