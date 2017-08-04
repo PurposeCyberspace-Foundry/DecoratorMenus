02ba21e6da2a3127c81e930e226c39f991686f2e
Matt Lombardo <egg@Matts-MacBook-Pro.local>
2017-08-04 00:08:21 -0500

    Clarified comments

--- DecoratorMenus.py
+++ DecoratorMenus.py
@@ -10,37 +10,36 @@ import argparse
 from ExceptionHiding import hide_last_exception
 
 
-# this could be replaced in the future to accept @argument decorators and provide default arguments for the program
+# this could be replaced in the future to accept @argument decorators and
+# provide default arguments for the program
 ap = argparse.ArgumentParser()
 
-# this could be replaced in the future as well, with another @function to create logical command groups
+# this could be replaced in the future as well, with another @function to create
+# logical command groups
 sp = ap.add_subparsers()
 
 # this will register a function or a function with arguments as a menu command
-# it would be really cool to have menu_item return a menu_item class which then could be added by the above
-# hypothetical future decorators
+# it returns the parser action, so theoretically you can do more with the
+# actiongroup
 def menu_item(*args, **kwargs):
     def next_item_processor(func):
-        sap = sp.add_parser(*args, **kwargs)
-        sap.set_defaults(func=func)
+        parser_action = sp.add_parser(*args, **kwargs)
+        parser_action.set_defaults(func=func)
         if hasattr(func, 'arguments'):
             for argument in func.arguments:
-                #print "added {} to {}".format((argument.args, argument.kwargs), func.__name__)
-                sap._add_action(argument)
+                parser_action._add_action(argument)
 
-        return sap
-        # one could concievably return the function, in which case the function would actually be defined
-        # for now, the function goes away once it is wrapped like this and the definition remains empty.
-        # return f
+        return parser_action
     return next_item_processor
 
-# this will add an argument to the function. It doesn't do anything on its own, but must follow menu_item
+# this will add an argument to the function. It doesn't do anything on its own,
+# but must follow menu_item
 def argument(*args, **kwargs):
     def next_item_processor(func):
         # create an action from an ephemeral ArgumentParser
         action = argparse.ArgumentParser().add_argument(*args, **kwargs)
 
-        #make sure that each action has a valid varname target
+        # make sure that each action has a valid varname parameter to match to
         if action.dest not in func.__code__.co_varnames:
             hide_last_exception()
             raise AttributeError(
@@ -58,7 +57,8 @@ def argument(*args, **kwargs):
 
     return next_item_processor
 
-# parses the args then runs the associated function passing it the named args as parameters
+# parses the args then runs the associated function passing it the
+# named args as parameters
 def run_menu():
     args = vars(ap.parse_args())
     func = args.pop("func")
